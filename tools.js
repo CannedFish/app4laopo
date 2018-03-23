1c3f32dc0f478657f2c6e89c9b5e6f1a8ee252a1
CannedFish <lianggy0719@126.com>
2018-03-23 16:11:22 +0800

    update tool

--- tools.js
+++ tools.js
@@ -3,35 +3,54 @@ const fs = require('fs');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(path.join(__dirname, './data/data.db'));
-const oldDis = path.join(__dirname, './data/dis.dat');
+
+function __insertDB(disData) {
+  db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS distance (src TEXT, dst TEXT, dis FLOAT)");
+    
+    let stmt = db.prepare("INSERT INTO distance VALUES (?, ?, ?)");
+    for(let i=0; i<disData.length; ++i) {
+      stmt.run(disData[i][0], disData[i][1], disData[i][2]);
+    }
+    console.log(`Start insert ${disData.length} records.`);
+    stmt.finalize();
+    console.log(`Finish insert records.`);
+    
+    db.get("SELECT COUNT(*) FROM distance", (err, row) => {
+      console.log(`Finish insert ${row} records.`);
+    });
+  });
+}
 
 function importDis() {
+  const oldDis = path.join(__dirname, './data/dis.dat');
   fs.readFile(oldDis, 'utf8', (err, data) => {
     if(err) {
       return ;
     }
 
     let disData = JSON.parse(data);
-    db.serialize(() => {
-      db.run("CREATE TABLE IF NOT EXISTS distance (src TEXT, dst TEXT, dis FLOAT)");
-      
-      let stmt = db.prepare("INSERT INTO distance VALUES (?, ?, ?)");
-      for(let i=0; i<disData.length; ++i) {
-        stmt.run(disData[i][0], disData[i][1], disData[i][2]);
-      }
-      console.log(`Start insert ${disData.length} records.`);
-      stmt.finalize();
-      console.log(`Finish insert records.`);
-      
-      db.get("SELECT COUNT(*) FROM distance", (err, row) => {
-        console.log(`Finish insert ${row} records.`);
-      });
-    });
+    __insertDB(disData);
   });
 }
 
+function daulDis() {
+}
+
 function main() {
-  importDis();
+  if(process.argv.length < 3) {
+    console.log("Usage: node tools.js [import|daul]");
+    process.exit(1);
+  }
+  
+  if(process.argv[2] == "import") {
+    importDis();
+  } else if(process.argv[2] == "daul") {
+    daulDis();
+  } else {
+    console.log("Usage: node tools.js [import|daul]");
+    process.exit(1);
+  }
 }
 
 main();
