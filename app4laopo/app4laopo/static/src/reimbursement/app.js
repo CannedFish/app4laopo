ccda401be135baab2d6bf4b6bd459b3e7f4ba2c9
CannedFish <lianggy0719@126.com>
2019-01-03 09:47:01 +0000

    finish result show

--- app4laopo/app4laopo/static/src/reimbursement/app.js
+++ app4laopo/app4laopo/static/src/reimbursement/app.js
@@ -4,7 +4,9 @@ var tabs = new Vue({
   el: '#nav-tabs',
   data: {
     searchTab: true,
-    mgtTab: false
+    mgtTab: false,
+    targetDistance: '',
+    results: []
   },
   methods: {
     toggle: (tabIdx, evt) => {
@@ -12,6 +14,64 @@ var tabs = new Vue({
       evt.stopPropagation();
       tabs.searchTab = (tabIdx === 0);
       tabs.mgtTab = (tabIdx === 1);
+    },
+    searchAction: (evt) => {
+      console.log(tabs.targetDistance)
+      tabs.results = [{
+        src: {
+          name_ch: '甲医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Jia',
+          id: '33928d120'
+        },
+        dst: {
+          name_ch: '乙医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Yi',
+          id: '33928d320'
+        },
+        distance: '24'
+      }, {
+        src: {
+          name_ch: '甲医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Jia',
+          id: '33928d120'
+        },
+        dst: {
+          name_ch: '乙医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Yi',
+          id: '33928d320'
+        },
+        distance: '25'
+      }, {
+        src: {
+          name_ch: '甲医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Jia',
+          id: '33928d120'
+        },
+        dst: {
+          name_ch: '乙医院',
+          lng: 0,
+          address: '',
+          lat: 0,
+          name_en: 'Hospital Yi',
+          id: '33928d320'
+        },
+        distance: '28'
+      }]
     }
   }
 });
